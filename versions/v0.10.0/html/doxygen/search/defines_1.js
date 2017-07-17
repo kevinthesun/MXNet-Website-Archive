1c8ef1cfa71249c10a3af4aed9e6c8dd78258f71
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-07-17 14:12:30 -0700

    Refactor

--- versions/v0.10.0/html/doxygen/search/defines_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['check_5fcuda_5ferror',['CHECK_CUDA_ERROR',['../cuda__utils_8h.html#afc69a418242c5b851993bc2307b1c897',1,'cuda_utils.h']]],
-  ['cublas_5fcall',['CUBLAS_CALL',['../cuda__utils_8h.html#a685d7ca3c9370ff471665abcacdeb381',1,'cuda_utils.h']]],
-  ['cuda_5fcall',['CUDA_CALL',['../cuda__utils_8h.html#a06cc7d24ca66505e69f5ad40009f5e8d',1,'cuda_utils.h']]],
-  ['curand_5fcall',['CURAND_CALL',['../cuda__utils_8h.html#a82d7233550780a8c186e79c24aed8406',1,'cuda_utils.h']]]
-];
--- versions/v0.10.0/html/doxygen/search/functions_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['add_5farguments',['add_arguments',['../classmxnet_1_1op_1_1SimpleOpRegEntry.html#a405bfb5929d459d45d93c2eaa645bfb5',1,'mxnet::op::SimpleOpRegEntry']]],
-  ['alloc',['Alloc',['../classmxnet_1_1Storage.html#aaa387e9ae8d66a63438543d640f6cfff',1,'mxnet::Storage']]],
-  ['asarray',['AsArray',['../classmxnet_1_1NDArray.html#ac3c424c05eabe0b7a4976191d81cf6b0',1,'mxnet::NDArray']]],
-  ['at',['At',['../classmxnet_1_1NDArray.html#addb42a7353a4c07b2d1bae0afa8ef48e',1,'mxnet::NDArray']]]
-];
